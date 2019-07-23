d323bda60dbb5ef82897d9a21f94897e07e43116
Vidalink <47756567+vidalink@users.noreply.github.com>
2019-07-23 16:25:29 -0300

    Add files via upload

---
+++ convert_test_cases.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+const oldFile = process.argv[2];
+const newFile = process.argv[3];
+
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync(oldFile));
+
+data.forEach(function(item){
+	item.responseMessage = JSON.stringify(item.responseMessage);
+	item.dataToCheck = JSON.stringify(item.dataToCheck);
+});
+
+fs.writeFileSync(newFile, JSON.stringify(data, null, 4));
